fa3c38293266687be9fdea647f05078087aeac7c
Tim Reed <tim@alloftheabove.co.uk>
2017-08-02 23:03:41 +0100

    begin to stroke path with dots - not good

--- src/main.js
+++ src/main.js
@@ -5,16 +5,16 @@ import './main.css'
 
 // JS vendor deps
 
-// Vars
+// // Vars
 let boxWidth = 300;
 let boxHeight = 300;
 let quantity = 3; //number of dots
 let duration = 0.001;  //duration (in seconds)
-let curveComplexity = 20; // number of points to draw curve from
+let curveComplexity = 5; // number of points to draw curve from
 
 // Set up the canvas
 let box = document.getElementById('canvas');
-var ctx = box.getctx('2d');
+var ctx = box.getContext('2d');
 box.className = 'box';
 ctx.canvas.width = boxWidth;
 ctx.canvas.height = boxHeight;
@@ -49,7 +49,7 @@ for (var i = 0; i < points.length; i++) {
     points[i] = {};
     points[i].x = coords.x;
     points[i].y = coords.y;
-    // plotPoints(coords);
+    plotPoints(coords);
 }
 
 // Set up the path on the canvas
@@ -66,6 +66,16 @@ for (i = 1; i < points.length - 2; i ++) {
     var xc = (points[i].x + points[i + 1].x) / 2;
     var yc = (points[i].y + points[i + 1].y) / 2;
     ctx.quadraticCurveTo(points[i].x, points[i].y, xc, yc);
+    
+    // Uncomment this to see dots drawn badly on curve
+    // draw the points
+    // for(var t=0;t<101;t+=1) {
+    //     // let to = setTimeout(function(y) {
+    //         // console.log(y);
+    //         setPoint(points[i-1], points[i], {x:xc, y:yc}, t);
+    //         // debugger;
+    //     // }, t * 3, t );
+    // }
 }
 // curve through the last two points
 ctx.quadraticCurveTo(points[i].x, points[i].y, points[i+1].x,points[i+1].y);
@@ -74,28 +84,36 @@ ctx.quadraticCurveTo(points[i].x, points[i].y, points[i+1].x,points[i+1].y);
 ctx.stroke();
 
 
-then = Date.now();
-setInterval(main, 1);
 
-var cube_x_position = 0;
+    var startPoint={x:50,y:150};
+    var controlPoint={x:125,y:20};
+    var endPoint={x:200,y:150};
 
-function main()
-{
-	ctx.beginPath();
-	ctx.moveTo(100, 20);
-	ctx.lineTo(200, 160);
-	ctx.quadraticCurveTo(230, 200, 250, 120);
-	ctx.bezierCurveTo(290, -40, 300, 200, 400, 150);
-	ctx.lineTo(500, 90);
-	ctx.lineWidth = 5;
-	ctx.strokeStyle = 'blue';
-	ctx.stroke();
+    ctx.beginPath();
+    ctx.moveTo(startPoint.x,startPoint.y);
+    ctx.quadraticCurveTo(controlPoint.x,controlPoint.y,endPoint.x,endPoint.y);
+    ctx.stroke();
 
-	ctx.fillRect(cube_x_position, 0, canvasHolder.width, canvasHolder.height);
+    function setPoint(startPoint, controlPoint,endPoint, index) {
+        var point = getQuadraticBezierXYatT(startPoint,controlPoint,endPoint,index/100);
+        ctx.beginPath();
+        ctx.arc(point.x,point.y,3,0,Math.PI*2);
+        ctx.closePath();
+        ctx.fillStyle = 'rgba(255, 165, 0, 0.1)';
+        ctx.fill();
+    }
 
-	if(cube_x_position < canvasHolder.width)
-	{
-		cube_x_position += 1;
-	}
+    // draw the points
+    for(var t=0;t<101;t+=1) {
+    //     setTimeout(function(y) {
+            // console.log(y);
+            setPoint(startPoint, controlPoint, endPoint, t);
+            // debugger;
+    //     }, t * 3, t );
+    }
 
-}
+    function getQuadraticBezierXYatT(startPt,controlPt,endPt,T) {
+        var x = Math.pow(1-T,2) * startPt.x + 2 * (1-T) * T * controlPt.x + Math.pow(T,2) * endPt.x; 
+        var y = Math.pow(1-T,2) * startPt.y + 2 * (1-T) * T * controlPt.y + Math.pow(T,2) * endPt.y; 
+        return( {x:x,y:y} );
+    }
